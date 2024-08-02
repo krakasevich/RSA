c73597b2ad74f093ce5a38bb534576b017148b60
Aleksandr <skrakasevic1@gmail.com>
2024-08-02 13:14:44 +0200

    Function to find the modular inverse. Generation of two different prime numbers. Calculating the value of Euler's function for n. Generating a random number e.

--- RSA.py
+++ RSA.py
@@ -1,7 +1,7 @@
 import math
 import random
 
-def prime_num(min_value, max_value):
+def generate_prime_num(min_value, max_value):
     prime = random.randint(min_value, max_value)
     while not is_prime(prime):
         prime = random.randint(min_value, max_value)
@@ -13,3 +13,25 @@ def is_prime(num):
         if num % i == 0:
             return False
     return True
+
+def mod_inverse(e, phi):
+    for d in range(3, phi_n):
+        if (d * e) % phi == 1:
+            return d
+    raise ValueError("Mod inverse does not exist.")
+
+p, q = generate_prime_num(100, 500), generate_prime_num(100, 500)
+
+while p == q:
+    q = generate_prime_num(100, 500)
+
+n = p * q
+phi_n = (p - 1) * (q - 1)
+
+e = random.randint(3, phi_n - 1)
+while math.gcd(e, phi_n) != 1:
+    e = random.randint(3, phi_n - 1)
+
+d = mod_inverse(e, phi_n)
+
+your_message = input("Enter your message to encrypt: ")
